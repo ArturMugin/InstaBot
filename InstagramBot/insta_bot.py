5a4da017441b5cf5c92bdb01659965df5a344bfb
ArturMugin <47824482+ArturMugin@users.noreply.github.com>
2019-12-23 12:19:31 +0000

    Added randmon file upload

--- InstagramBot/insta_bot.py
+++ InstagramBot/insta_bot.py
@@ -1,17 +1,30 @@
 from instabot import Bot
-
+import os
+import random
+import time
 
 league = Bot()
 #login to insta
 league.login(username="League_boomers", password="leagueoglegendsinsta123")
 def upload():
     #choose which pic and caption to post
-    league.upload_photo("pic.jpg", caption="This is my first post!")
+    path =r'C:\Users\Mr Kek\Desktop\Insta bot\RedditBot\images\\'
+    path_for_delete =r'C:\Users\Mr Kek\Desktop\Insta bot\RedditBot\images'
+    files = os.listdir(path)
+    index = random.randrange(0, len(files))
+    print(files[index])
 
 
-while True:
-    try:
-        upload()
-        time.sleep(7200)
-    except KeyboardInterrupt:
-        print('Manual break by user')
+
+    #choose a ramd,om file from folder and assign caption as a name of the file (delete 4 characters .jpg)
+    league.upload_photo(path + files[index], caption=files[index][:-4])
+
+
+
+#    time.sleep(5)
+#    os.remove(path_for_delete + files[index] + ".REMOVE_ME")
+
+
+
+upload()
+
