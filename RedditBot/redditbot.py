1416fff1c0b59f317137a6b98ea3d637a576b53a
Kyle McPherson <kylejmcpherson@gmail.com>
2019-12-24 05:32:03 +0000

    - Softcoded the file path the image is saved to. (was hardcoded before)
    - Images are now saved as the correct filetype
    
    - Minor code changes

--- RedditBot/redditbot.py
+++ RedditBot/redditbot.py
@@ -1,54 +1,36 @@
 import praw
 import urllib.request
-
-#list of post titles
-mylist_new = list()
+import os
 
 
+# Download the image and set the filename
+def downloadImage(url, file_name):
+    urllib.request.urlretrieve(url, f"{os.getcwd()}\\images\\{file_name}.{url[-4:]}")
 
-#name the jpg file and download it
-def dl_jpg(url, file_path, file_name):
-    full_path = file_path + file_name + '.jpg'
-    urllib.request.urlretrieve(url, full_path)
+# Pre-process the image for downloading
+def handleImages(leagueOfMemes):
+    for submission in leagueOfMemes:
+        # Delete Windows restricted characters
+        fileName = submission.title.translate({ord(i): None for i in '!,./?*"!;][=+'})
 
-
-
-
-
-# reddit api login
-reddit = praw.Reddit(client_id='_tcBKu9u-1WnAg',
-                     client_secret='lA_WEyYyVTZyFFVBKO98QOBzWKM',
-                     username='leagueoglegendsinsta',
-                     password='leagueoglegendsinsta123',
-                     user_agent='League memes')
+        # Download the image
+        downloadImage(submission.url, fileName)
 
 
+def main():
+    # Reddit API login
+    reddit = praw.Reddit(client_id='_tcBKu9u-1WnAg',
+                         client_secret='lA_WEyYyVTZyFFVBKO98QOBzWKM',
+                         username='leagueoglegendsinsta',
+                         password='leagueoglegendsinsta123',
+                         user_agent='League memes')
 
-def download():
-    #choose subredit
-    subreddit = reddit.subreddit('LeagueOfMemes')
+    # Choose Sub-Reddit
+    subReddit = reddit.subreddit('LeagueOfMemes')
 
-    #take top five posts per day
-    LeagueOfMemes = subreddit.top(time_filter='day', limit=6)
+    # Get top 5 posts
+    leagueOfMemes = subReddit.top(time_filter="day", limit=5)
 
-
-    # function to be done with the posts
-    for submission in LeagueOfMemes:
-        #delete windows restricted characters for files
-        file_name = submission.title.translate({ord(i): None for i in '!,./?*"!;][=+'})
-        #print the title of post
-        print(file_name)
-        url = submission.url
-        #take iamge url of a post
-        dl_jpg(url, 'images/', file_name)
-        #add post title to list
-        mylist_new.append(submission.title)
-
-
-
-
-#start program
-download()
-#print the list
-print(mylist_new)
-
+    # Handle the response of the top 5 posts
+    handleImages(leagueOfMemes)
+main()
